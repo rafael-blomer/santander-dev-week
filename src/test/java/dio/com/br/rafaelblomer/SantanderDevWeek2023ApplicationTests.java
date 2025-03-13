b46ded2a69db28132f6397975358b843957fdd59
rafael-blomer <rafael.blomer@gmail.com>
2025-03-13 14:25:05 -0300

    Initial commit

---
+++ src/test/java/dio/com/br/rafaelblomer/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.com.br.rafaelblomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
